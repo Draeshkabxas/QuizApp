88cbe5a5b807a0c173a79f68a9e7a1d7d790507b
derar <zzxxccz908@gmail.com>
2021-07-19 06:08:25 +0200

    Make MainActivity.kt fullscreen

--- app/src/main/java/com/derarlibya/quizapp/MainActivity.kt
+++ app/src/main/java/com/derarlibya/quizapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.derarlibya.quizapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.derarlibya.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //makeFullScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AndroidBug5497Workaround.assistActivity(this)
+
+    }
+
+
+    private fun makeFullScreen(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
