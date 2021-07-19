ccc17dbf81b39ea7c2eb7f9d29d1b0d74bbc7f75
derar <zzxxccz908@gmail.com>
2021-07-19 06:31:43 +0200

    Create QuizQuestionsActivity.kt and make start button start it

--- app/src/main/java/com/derarlibya/quizapp/MainActivity.kt
+++ app/src/main/java/com/derarlibya/quizapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.derarlibya.quizapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.derarlibya.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,14 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         AndroidBug5497Workaround.assistActivity(this)
 
+        //when user click on start button run onStartClicked function
+        binding.btnStart.setOnClickListener { view ->
+            onStartClicked(view )
+        }
+
     }
 
-
-    private fun makeFullScreen(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-        } else {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    /**
+     * this method check if user add a name
+     * if them add one then start new activity and finish this activity.
+     * if not it show toast Please enter your name. and do nothing else.
+     */
+    fun onStartClicked(view: View){
+        if(binding.etName.text.isNullOrEmpty()){
+            Toast.makeText(this,"Please enter your name.",Toast.LENGTH_SHORT).show()
+            return
         }
+        val intent=Intent(this,QuizQuestionsActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
