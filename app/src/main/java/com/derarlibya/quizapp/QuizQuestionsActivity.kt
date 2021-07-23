a137148cd6d2503a65a9ff3d07a74e8b52a1696f
derar <zzxxccz908@gmail.com>
2021-07-23 05:39:32 +0200

    Add the ability to select option
    Add select_option_border_bg.xml that will be the selected drawable
    Add several methods to make the ability of select option and these method are setOptionsOnClickListener , setOptions ,
    defaultOptionView ,onClick and selectedOptionView.

--- app/src/main/java/com/derarlibya/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/derarlibya/quizapp/QuizQuestionsActivity.kt
@@ -1,54 +1,112 @@
 package com.derarlibya.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
-import androidx.core.content.ContextCompat
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.derarlibya.quizapp.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var binding: ActivityQuizQuestionsBinding
-    var index=4
+    private lateinit var options: MutableList<TextView>
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
+    private var currentQuestion: Int = 4
+    private var questionsList: List<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val questionsList = Constants.getQuestions()
+        questionsList = Constants.getQuestions()
 
-        questionsList.forEach { question ->
-            Log.i("QuestionsList ", "$question")
-        }
-        show(questionsList)
+        options=ArrayList<TextView>()
 
+        setQuestion()
+
+        setOptions()
+
+        setOptionsOnClickListener()
     }
 
 
-    private fun show(questionsList:List<Question>){
-        val currentQuestion=questionsList[index]
+    private fun setOptionsOnClickListener(){
+        options.forEach { options ->
+            options.setOnClickListener(this)
+        }
+    }
 
-        //Set question textView to be the question of currentQuestion
-        binding.tvQuestion.text=currentQuestion.question
 
-        //Set image ImageView to be the image of currentQuestion
-        binding.ivImage.setImageResource( currentQuestion.image)
+    @SuppressLint("SetTextI18n")
+    private fun setQuestion() {
+        val mQuestion = questionsList!![currentQuestion]
+
+        //Set all option to the default
+        defaultOptionView()
+
+        //Set question textView to be the question of mQuestion
+        binding.tvQuestion.text = mQuestion.question
+
+        //Set image ImageView to be the image of mQuestion
+        binding.ivImage.setImageResource(mQuestion.image)
 
         // index start from 0 but progress should start from 1
-        val currentProgress=index+1
-        //Set progress progressBar to be currentProgress
+        val currentProgress = currentQuestion + 1
+        //Set progress progressBar to be mQuestion
 
-        binding.progressBar.progress=currentProgress
+        binding.progressBar.progress = currentProgress
 
-        //Set progress text TextView to be currentProgress/how many questions
-        binding.tvProgress.text="$currentProgress/${questionsList.size}"
+        //Set progress text TextView to be mQuestion/how many questions
+        binding.tvProgress.text = "$currentProgress/${questionsList!!.size}"
 
 
-        //Set option questions textView to be the questions of currentQuestion
-        binding.tvOptionOne.text=currentQuestion.optionOne
-        binding.tvOptionTwo.text=currentQuestion.optionTwo
-        binding.tvOptionThree.text=currentQuestion.optionThree
-        binding.tvOptionFour.text=currentQuestion.optionFour
+        //Set option questions textView to be the questions of mQuestion
+        binding.tvOptionOne.text = mQuestion.optionOne
+        binding.tvOptionTwo.text = mQuestion.optionTwo
+        binding.tvOptionThree.text = mQuestion.optionThree
+        binding.tvOptionFour.text = mQuestion.optionFour
+
+    }
+
+    private fun setOptions(){
+        options.add(binding.tvOptionOne)
+        options.add(binding.tvOptionTwo)
+        options.add(binding.tvOptionThree)
+        options.add(binding.tvOptionFour)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun defaultOptionView() {
+        options.forEach { option ->
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = getDrawable(R.drawable.default_option_border_bg)
+        }
+    }
+
+
+    override fun onClick(p0: View?) {
+         for (i in 0 until options.size){
+             if (options[i]==p0) {
+                 selectedOptionView(p0 as TextView, i+1)
+             Log.i(this.localClassName,"Selected option is $i")
+             }
+         }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun selectedOptionView(tv: TextView
+                                   , selectedOptionNum: Int) {
+        defaultOptionView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.background = getDrawable(R.drawable.select_option_border_bg)
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface,Typeface.BOLD)
 
     }
 
